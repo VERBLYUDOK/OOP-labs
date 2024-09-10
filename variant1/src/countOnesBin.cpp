c0694d209561991f7d7d67f96cdab28c67912289
Kirbass <kirillasz3@gmail.com>
2024-09-10 19:38:38 +0300

    added coments

--- variant1/src/countOnesBin.cpp
+++ variant1/src/countOnesBin.cpp
@@ -4,6 +4,10 @@ int countOnesBin(int a, int b) {
     int result = 0;
     for (int i = a; i <= b; ++i) {
 	result += __builtin_popcount(i); // встроенная функция в gcc для подсчета единиц в двоичном представлении числа
+    //    while (i) {
+    //        if (i%2 == 1) result += 1;
+    //        i /= 2;
+    //    }
     }
     return result;
 }
